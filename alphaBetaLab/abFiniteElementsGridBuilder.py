faee3af3951ca4d5fc5b164ffd217b6d001fd194
Lorenzo Mentaschi <lorenzo.mentaschi@unige.it>
2018-02-25 11:43:13 +0100

    abFiniteElementsGridBuilder, test complete

--- alphaBetaLab/abFiniteElementsGridBuilder.py
+++ alphaBetaLab/abFiniteElementsGridBuilder.py
@@ -1,15 +1,16 @@
 import abGrid
 
 
-class abRectangularGridBuilder:
+class abFiniteElementsGridBuilder:
 
   def __init__(self, feMeshSpec, nParWorker=4):
     self.feMeshSpec = feMeshSpec
     self.nParWorker = nParWorker
 
   def buildGrid(self):
-    nodeIds, cells = self.feMeshSpec.getCellPolygons(excludeLandBoundary=True)
-    cellcrds = [(nid - 1, 0) for nid in nodeIds]
+    nodeIds, cells = self.feMeshSpec.getCellPolygons(excludeLandBoundary=True,
+                                                     excludeOpenBoundary=False)
+    cellcrds = [(nid, 1) for nid in nodeIds]
     grd = abGrid.getLandSeaGrid(cells, cellcrds, nParWorker=self.nParWorker)
     grd.isRegular = False
 
