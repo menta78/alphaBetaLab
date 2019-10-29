06feea91d31d76122cf4fe018607ff05fac06e77
giospud <giospud@gmail.com>
2019-10-29 16:46:21 +0100

    changed the setup file: eliminated the installatioin of the "test" package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.1.0',
     author='Lorenzo Mentaschi',
     author_email='lorenzo.mentaschi@unige.it',
-    packages=['alphaBetaLab', 'alphaBetaLab.test', 'alphaBetaLab.plot'],
+    packages=['alphaBetaLab', 'alphaBetaLab.plot'],
     install_requires=[
         "numpy >= 1.8.2",
         "shapely >= 1.5.9",
