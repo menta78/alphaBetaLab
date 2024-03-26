611ca412d35cb9d4d304abf9bf056516d7d61982
Lorenzo Mentaschi <lorenzo.mentaschi@unibo.it>
2024-03-26 23:18:50 +0100

    removed the dependencies from setup.py (assuming one follows the wiki to install)

--- setup.py
+++ setup.py
@@ -6,11 +6,5 @@ setup(
     author='Lorenzo Mentaschi',
     author_email='lorenzo.mentaschi@unige.it',
     packages=['alphaBetaLab', 'alphaBetaLab.plot'],
-    install_requires=[
-        "numpy >= 1.8.2",
-        "shapely >= 1.5.9",
-        "basemap",
-        "pyproj <= 5.2"
-    ],
 )
  
